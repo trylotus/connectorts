fe419de94a7368e8532f68f9e3c5b9f1a2ecf569
Cenk Arioz <cenkarioz@gmail.com>
2022-10-24 21:21:21 +0300

    create manifest and config

---
+++ src/manifest.ts
@@ -0,0 +1,29 @@
+import { SemVer } from 'semver/classes/semver'
+import { load } from 'js-yaml'
+import { readFileSync } from 'fs'
+
+export type Manifest = {
+    name: string
+    author: string
+    version: typeof SemVer
+}
+
+export function loadManifest(): Manifest {
+    console.log("loading manifest")
+
+    let yfile
+    try {
+        yfile = load(readFileSync("manifest.yaml", 'utf8'))
+    } catch (e) {
+        console.log("failed to open file manifest.yaml. error: " + e)
+    }
+
+    let manifest = yfile
+
+    if (!manifest.name || !manifest.author || !manifest.version) {
+        console.log("missing name, author, and version fields from manifest.yaml.")
+    }
+
+    console.log("name: " + manifest.name + " author: " + manifest.author + " version: " + manifest.version)
+    return manifest
+}
